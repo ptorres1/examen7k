402699f21c0caaca89710ee223358766ef5a93d4
Paloma Torres <zy25rac@gmail.com>
2017-12-13 04:33:39 +0000

    Agregando teléfono a la BD Issue #2

--- datos.sql
+++ datos.sql
@@ -2,6 +2,7 @@ CREATE TABLE `datos` (
   `id` int(8) unsigned NOT NULL auto_increment,
   `nombre` varchar(70) default NULL,
   `direccion` varchar(120) default NULL,
+  `telefono` int(10) default NULL,
   `fechaCreacion` timestamp NOT NULL,
   PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
